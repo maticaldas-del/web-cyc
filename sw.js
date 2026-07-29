0203e92514e591f59112d429f15f590b30a22afc
maticaldas-del <chivilcoyenvioscyc@hotmail.com>
2026-07-29 17:58:27 +0000

    Resumen: mostrar que el costo de lo vendido ya incluye IIBB y monotributo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker de CYC: cachea la app (mismo origen) para que abra como app instalada.
-const CACHE = "cyc-v55";
+const CACHE = "cyc-v56";
 const ARCHIVOS = [".", "index.html", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (e) => {
